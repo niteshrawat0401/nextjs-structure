75ba4bd56cf6a4cce469dc37e1594065b52651ec
niteshrawat0401 <niteshrawat0401@gmail.com>
2023-01-18 23:14:04 +0530

    api fetch

--- pages/users/index.jsx
+++ pages/users/index.jsx
@@ -5,20 +5,25 @@ export default function Users({ users }) {
 
   return (
     <div>
-      {users.map((u) => (
-        <div key={u.id}>
-          {u.id}. {u.name}
-        </div>
-      ))}
+
     </div>
   );
 }
 
-export const getServerSideProps = async () => {
-  const data = await fetch("https://jsonplaceholder.typicode.com/users").then(
-    (x) => x.json()
-  );
+// export const getServerSideProps = async () => {
+//   const data = await fetch("https://jsonplaceholder.typicode.com/users").then(
+//     (x) => x.json()
+//   );
+//   return {
+//     props: { users: data },
+//   };
+// };
+
+export const getServerSideProps = async()=>{
+  const res= await fetch("https://jsonplaceholder.typicode.com/users")
+  const data= await res.json()
+
   return {
-    props: { users: data },
-  };
-};
+    props: {users: data}
+  }
+}
