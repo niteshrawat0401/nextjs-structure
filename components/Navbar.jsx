9157628839dd7b0fc1283db52e9c537af57e7a3e
niteshrawat0401 <niteshrawat0401@gmail.com>
2023-01-19 00:07:44 +0530

    link

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,13 +1,21 @@
 import React from 'react'
+import Link from 'next/link'
 
 export const Navbar = () => {
   return (
+    // <div style={{
+    //     display: 'flex',
+    //     gap: 10}}>
+    //     <a href=''>HOME</a>
+    //     <a href=''>ABOUT</a>
+    //     <a href=''>GALLARY</a>
+    // </div>
     <div style={{
         display: 'flex',
         gap: 10}}>
-        <a href=''>HOME</a>
-        <a href=''>ABOUT</a>
-        <a href=''>GALLARY</a>
+        <Link href=''>HOME</Link>
+        <Link href='/About'>ABOUT</Link>
+        <Link href='/gallary'>GALLARY</Link>
     </div>
   )
 }
