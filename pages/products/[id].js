d15e8bc594aed8dc424358b4f59a0c2380e6a0e9
niteshrawat0401 <niteshrawat0401@gmail.com>
2023-01-19 00:07:27 +0530

    folder inside folder route

--- pages/products/[id].js
+++ pages/products/[id].js
@@ -2,6 +2,6 @@ import { useRouter } from "next/router"
 
 export default function ProductsDetails() {
     const { query }= useRouter();
-    return (<div>product details of id: {query.id}</div>
+    return (<div>Products details of id: {query.id}</div>
     )
   }
---
+++ pages/products/userproduct/[userpro].js
@@ -0,0 +1,14 @@
+import { useRouter } from 'next/router'
+import React from 'react'
+
+const userpro = () => {
+    const router= useRouter();
+    // console.log({router});
+    const {userpro}= router.query;
+
+  return (
+    <div>userpro: {userpro}</div>
+  )
+}
+
+export default userpro
