510318e89211149edf3060b476e09dc338365d58
niteshrawat0401 <niteshrawat0401@gmail.com>
2023-01-20 00:59:41 +0530

    show data through id

--- pages/users/[id].js
+++ pages/users/[id].js
@@ -1,13 +1,27 @@
-import React from 'react'
+import React from "react";
 
-const showData = () => {
+const showData = ({ data }) => {
   return (
-    <div>[id]</div>
-  )
-}
+    <div>
+      <h3>{data.name}</h3>
+      <h3>{data.email}</h3>
+    </div>
+  );
+};
 
-export default showData
+export const getServerSideProps = async (context) => {
+  try {
+    const res = await fetch(
+      `https://jsonplaceholder.typicode.com/users/${context.query.id}`
+    );
+    const data = await res.json();
+    console.log(data);
+    return {
+      props: { data },
+    };
+  } catch (error) {
+    console.log(error);
+  }
+};
 
-export const getServerSideProps=()=>{
-  
-}
+export default showData;
